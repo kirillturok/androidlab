d53a1e737141071dba0c4b0b8e1db39859c9a526
kirillturok <71722037+kirillturok@users.noreply.github.com>
2021-07-05 16:19:41 +0300

    Update Combinator.kt

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -4,6 +4,21 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        var n_fact = fact(array[1])
+        for(i in 1..array[1]){
+            var res = n_fact/(fact(i)*fact(array[1]-i))
+            if(res.toInt() == array[0]){
+                return i
+            }
+        }
+        return null
+    }
+
+    private fun fact(n:Int):Long{
+        var res:Long=1;
+        for (i in 1..n){
+            res*=i
+        }
+        return res
     }
 }
