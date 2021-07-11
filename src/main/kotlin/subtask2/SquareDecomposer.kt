f47f6de40eff1c549a5daaacf86de9b479b938a2
kiril <kirill>
2021-07-11 21:15:37 +0300

    last

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -4,6 +4,22 @@ class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return getSquares(number, square(number))
+    }
+
+    private fun square(i:Int): Long = (i.toLong() * i.toLong())
+
+    private fun getSquares(number: Int, total: Long): Array<Int>? {
+        for (i in number - 1 downTo 1) {
+            if (square(i) == total)
+                return Array(1) { i }
+
+            val diff = total - square(i)
+            if (diff > 0) {
+                val arr = getSquares(i, diff)
+                if (arr != null) return arr.plus(i)
+            }
+        }
+        return null
     }
 }
--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -4,6 +4,29 @@ class TelephoneFinder {
 
     // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        if(number.toInt()<=0) return null
+        val result = mutableListOf<String>()
+        number.forEachIndexed { index, c ->
+            val neighbour = neighbors(c)
+            neighbour.forEach { cNew ->
+                val newNumber = number.replaceRange(index, index+1, cNew.toString())
+                result.add(newNumber)
+            }
+        }
+        return if (result.isNotEmpty()) result.toTypedArray() else null
+    }
+
+    private fun neighbors(num: Char) = when(num) {
+        '1' -> arrayOf('2', '4')
+        '2' -> arrayOf('1', '3', '5')
+        '3' -> arrayOf('2', '6')
+        '4' -> arrayOf('1', '5', '7')
+        '5' -> arrayOf('2', '4' , '6' ,'8')
+        '6' -> arrayOf('3', '5', '9')
+        '7' -> arrayOf('4', '8')
+        '8' -> arrayOf('5', '7', '9', '0')
+        '9' -> arrayOf('6', '8')
+        '0' -> arrayOf('8')
+        else -> arrayOf()
     }
 }
